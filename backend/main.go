871b9d09b84bd55598440a7820922e90b2c6802e
Daire Homan <dairehoman@gmail.com>
2018-02-17 16:17:38 +0000

    init

---
+++ backend/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"flag"
+	"net"
+	"fmt"
+	"github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	pb "../api"
+	"golang.org/x/net/context"
+	"os/exec"
+	"io/ioutil"
+)
+
+func main() {
+	port := flag.Int("p", 8080, "port to listen to")
+	flag.Parse()
+
+	logrus.Infof("listening to port %d", *port)
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		logrus.Fatalf("could not listen to port %d: %v", *port, err)
+	}
+
+	s := grpc.NewServer()
+	pb.RegisterTextToSpeechServer(s, server{})
+	s.Serve(lis)
+	if err != nil {
+		logrus.Fatalf("could not serve: %v", err)
+	}
+}
+
+type server struct{}
+
+func (server) Say(ctx context.Context, text *pb.Text) (*pb.Speech, error) {
+	f, err := ioutil.TempFile("", "")
+	if err != nil {
+		return nil, fmt.Errorf("could not create tmp file: %v", err)
+	}
+	if err := f.Close(); err != nil {
+		return nil, fmt.Errorf("could not close %s: %v", f.Name(), err)
+	}
+
+	cmd := exec.Command("flite", "-t", text.Text, "-o", f.Name())
+	if data, err := cmd.CombinedOutput(); err != nil {
+		return nil, fmt.Errorf("flite failed: %s", data)
+	}
+
+	data, err := ioutil.ReadFile(f.Name())
+	if err != nil {
+		return nil, fmt.Errorf("could not read tmp file: %v", err)
+	}
+	return &pb.Speech{Audio: data}, nil
+}
